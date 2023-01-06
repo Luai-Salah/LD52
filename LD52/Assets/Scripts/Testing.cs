4486f8a6c988ca349bbf88fcaa09eda49307f285
Luai-Salah <77834783+Luai-Salah@users.noreply.github.com>
2023-01-06 20:01:20 +0300

    Test

--- LD52/Assets/Scripts/Testing.cs
+++ LD52/Assets/Scripts/Testing.cs
@@ -1,16 +1,8 @@
-using System;
 using UnityEngine;
 
 namespace LD52
 {
     public class Testing : MonoBehaviour
     {
-        private void Start()
-        {
-        }
-
-        private void Update()
-        {
-        }
     }
 }
