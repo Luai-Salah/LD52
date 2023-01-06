8b7c676907a040c6ea3c824cab779bf3c5da2ca6
Luai-Salah <77834783+Luai-Salah@users.noreply.github.com>
2023-01-06 19:49:51 +0300

    Initialize Project

---
+++ LD52/Assets/Scripts/Testing.cs
@@ -0,0 +1,16 @@
+using System;
+using UnityEngine;
+
+namespace LD52
+{
+    public class Testing : MonoBehaviour
+    {
+        private void Start()
+        {
+        }
+
+        private void Update()
+        {
+        }
+    }
+}
